eeeb1c7113d59f0d4312fc19a6e2e93ed225b7fb
DaniAZ <da16ni19@gmail.com>
2020-05-19 19:36:02 -0400

    change

--- applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/NoOauthResourceServerConfig.java
+++ applications/backlog-server/src/main/java/io/pivotal/pal/tracker/backlog/NoOauthResourceServerConfig.java
@@ -15,7 +15,7 @@ public class NoOauthResourceServerConfig extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
     }
 
     @Bean
